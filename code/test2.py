4d0070dc699dd677ed08ad468a0a5af87ec4381d
itmorn <12567148@qq.com>
2022-06-07 16:14:45 +0800

    改为合速度前备份

---
+++ code/test2.py
@@ -0,0 +1,26 @@
+# -*-coding:utf-8-*-
+
+def init_test():
+    '''
+    用于初始化项目测试，不需要任何参数
+    :return:
+    '''
+    print("初始化项目")
+
+
+def start():
+    '''
+    启动项目入口，
+    :return:
+    '''
+    test(3)
+
+
+def test(v):
+    '''
+    项目运行主要函数，需要传入一个参数\n
+    v:<int>
+    :return:
+    '''
+
+    print(v)
