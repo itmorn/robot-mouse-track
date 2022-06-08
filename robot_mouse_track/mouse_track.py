0ef945e9f433b351ae19e048ad50f828540a13e7
itmorn <12567148@qq.com>
2022-06-08 11:03:23 +0800

    梯度计算不返回时间，方向角，调整结构

--- robot_mouse_track/mouse_track.py
+++ robot_mouse_track/mouse_track.py
@@ -3,29 +3,19 @@ from trace import trace
 import numpy as np
 import matplotlib.pyplot as plt
 
+DECOMPOSITION = "decomposition"
+COMBINE = "combine"
+
 
 class MouseTrajectory:
     def __init__(self, trace):
         self.arr_trace = np.array(trace, np.float64)
+        self.arr_time = self.arr_trace[:, -1]
 
         self.max_duration_silent = 20  # 间隔超过多少秒，算静止，将长间隔缩短为一个固定长度
+        self.max_DOA = 20  # 正切值峰值截断
 
-    # def get_feature_dev_partial(self, order=2):
-    #     """
-    #     获取偏导数特征
-    #     :param order: 求导阶数
-    #     :return:list
-    #     """
-    #     arr_dev = self.arr_trace
-    #     lst_feature_dev_partial = []
-    #     for _ in range(1, order + 1):
-    #         arr_diff = arr_dev[1:, :] - arr_dev[:-1, :]
-    #         arr_dev = arr_diff[:, :-1] / np.clip(arr_diff[:, -1:], 0, self.max_duration_silent)
-    #         arr_dev = np.c_[arr_dev, self.arr_trace[:len(arr_dev), -1]]
-    #         lst_feature_dev_partial.append(arr_dev)
-    #     return lst_feature_dev_partial
-
-    def get_feature_dev(self, order=2, mode="all"):
+    def get_feature_dev(self, order=2, mode=COMBINE):
         """
         获取导数特征
         :param order: 求导阶数
@@ -35,16 +25,18 @@ class MouseTrajectory:
 
         arr_diff_dis = self.arr_trace[1:, :] - self.arr_trace[:-1, :]
         arr_diff_time = arr_diff_dis[:, -1:]
-        if mode == "all":
+        if mode == COMBINE:
             arr_diff_dis = (np.sum((arr_diff_dis[:, :-1] ** 2), axis=1) ** 0.5)[:, np.newaxis]
-        else:
+        elif mode == DECOMPOSITION:
             arr_diff_dis = arr_diff_dis[:, :-1]
+        else:
+            raise Exception("请输入正确的类型")
 
         order_cur = 1
         while 1:
             arr_dev = arr_diff_dis / arr_diff_time[:len(arr_diff_dis)]
-            arr = np.c_[arr_dev, self.arr_trace[:len(arr_dev), -1]]
-            lst_feature_dev.append(arr)
+            # arr = np.c_[arr_dev, self.arr_trace[:len(arr_dev), -1]]
+            lst_feature_dev.append(arr_dev)
             order_cur += 1
             if order_cur > order:
                 break
@@ -63,16 +55,27 @@ class MouseTrajectory:
         # plt.ylim(0, 2000)
         plt.show()
 
+    def get_feature_DOA(self):
+        arr_diff_dis = self.arr_trace[1:, :-1] - self.arr_trace[:-1, :-1]
+        # 防止除以0
+        arr_diff_dis[:, 0][np.where(arr_diff_dis[:, 0] == 0)] = 10e-8
+        arr_DOA = np.clip(arr_diff_dis[:, 1] / arr_diff_dis[:, 0], -self.max_DOA, self.max_DOA)
+        return arr_DOA
+
 
 if __name__ == '__main__':
     mta = MouseTrajectory(trace)
     mta.show_track()
 
-    lst_feature = mta.get_feature_dev(order=3, mode="") #all
-
-    for arr_feature in lst_feature:
-        plt.figure()
-        x_time = arr_feature[:, -1]
-        y_dev = arr_feature[:, 0]
-        plt.plot(x_time, y_dev)
-        plt.show()
+    # lst_feature = mta.get_feature_dev(order=3, mode=COMBINE)  # DECOMPOSITION
+    #
+    # for arr_feature in lst_feature:
+    #     plt.figure()
+    #     plt.plot(mta.arr_time[:len(arr_feature)], arr_feature[:, 0])
+    #     plt.show()
+    arr_DOA = mta.get_feature_DOA()
+    plt.figure(figsize=(10, 10))
+    # plt.plot(mta.arr_time[:100], arr_DOA[:100])
+    plt.plot(mta.arr_time[:len(arr_DOA)], arr_DOA)
+    print(arr_DOA)
+    plt.show()
